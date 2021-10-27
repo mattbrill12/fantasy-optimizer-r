27530bd74ea30cc659bd7ff3cc43bcff49a4080a
Hugh Goode <hjgoode3@gmail.com>
2021-10-27 19:12:48 -0400

    add DK data table

--- backend/DK_player_data.R
+++ backend/DK_player_data.R
@@ -7,19 +7,27 @@ week_current <- hist_df %>% filter(Week == 7)
 
 player_proj <- week_current %>% select(Name, Pos, Team, Games.Played, Average.DK.Points)
 
-json_file <- 'https://api.draftkings.com/draftgroups/v1/draftgroups/58074/draftables?format=json'
+json_file <- 'https://api.draftkings.com/draftgroups/v1/draftgroups/58073/draftables?format=json'
 json_data <- fromJSON(file=json_file)
 
 draftables = json_data$draftables
 
+LastName <- c()
+FirstName <- c()
+Position = c()
+Salary = c()
+Proj_Points = c()
 
-
-for (i in 1:length(draftables)){
-  print(draftables[i])
-}
-l <-c()
 for(i in 1:length(draftables)){
- 
-  l[i] =  draftables[[i]]$firstName
+  FirstName[i] =  draftables[[i]]$firstName
+  LastName[i] =  draftables[[i]]$lastName
+  Position[i] =  draftables[[i]]$position
+  Salary[i] =  draftables[[i]]$salary
+  if (draftables[[i]]$draftStatAttributes[[1]]$id >= 0){ 
+  Proj_Points[i] =  draftables[[i]]$draftStatAttributes[[1]]$value}
+  else {Proj_Points[i] = NA}
 }
-l
+
+DK_DF = data.frame(LastName=LastName, FirstName = FirstName, Position = Position, Salary = Salary, Proj_Points = Proj_Points)
+
+DK_DF = unique(DK_DF)
