af902c02dbc9c7a260e23f06a743f5a347aeec75
Matthew Brillantes <mattbrill12@gmail.com>
2021-10-27 22:26:35 -0400

    merge

--- backend/DK_player_data.R
+++ backend/DK_player_data.R
@@ -7,6 +7,7 @@ week_current <- hist_df %>% filter(Week == 7)
 
 player_proj <- week_current %>% select(Name, Pos, Team, Games.Played, Average.DK.Points)
 
+
 # Split Name column into first and last name inorder to merge with DK dataframe later
 lastname = sapply(strsplit(player_proj$Name, ','), function(x) x[1])
 firstname = sapply(strsplit(player_proj$Name, ','), function(x) x[length(x)])
@@ -20,8 +21,20 @@ lastname = gsub(' I', '', lastname, fixed = T)
 player_proj['Firstname'] = firstname
 player_proj['Lastname'] = lastname
 
-#Rename Def positions to DST in order to match DK data frame data
+#Rename Def positions to DST in order to match DK data frame
 player_proj$Pos = gsub('Def', 'DST', player_proj$Pos, fixed = T)
+
+#Rename team abbr. in order to match DK data frame
+player_proj$Team = gsub('gnb', 'gb', player_proj$Team, fixed = T)
+player_proj$Team = gsub('kan', 'kc', player_proj$Team, fixed = T)
+player_proj$Team = gsub('nwe', 'ne', player_proj$Team, fixed = T)
+player_proj$Team = gsub('nor', 'no', player_proj$Team, fixed = T)
+player_proj$Team = gsub('sfo', 'sf', player_proj$Team, fixed = T)
+player_proj$Team = gsub('tam', 'tb', player_proj$Team, fixed = T)
+player_proj$Team = gsub('jac', 'jax', player_proj$Team, fixed = T)
+
+
+player_proj
 
 json_file <- 'https://api.draftkings.com/draftgroups/v1/draftgroups/58073/draftables?format=json'
 json_data <- fromJSON(file=json_file)
@@ -33,18 +46,27 @@ FirstName <- c()
 Position = c()
 Salary = c()
 Proj_Points = c()
+Team = c()
 
 for(i in 1:length(draftables)){
   FirstName[i] =  draftables[[i]]$firstName
   LastName[i] =  draftables[[i]]$lastName
   Position[i] =  draftables[[i]]$position
   Salary[i] =  draftables[[i]]$salary
-  if (draftables[[i]]$draftStatAttributes[[1]]$id >= 0){ 
+  Team[i] =  tolower(draftables[[i]]$teamAbbreviation)
+  if (draftables[[i]]$draftStatAttributes[[1]]$id >= 0){
   Proj_Points[i] =  draftables[[i]]$draftStatAttributes[[1]]$value}
   else {Proj_Points[i] = NA}
 }
 
-DK_DF = data.frame(LastName=LastName, FirstName = FirstName, Position = Position, Salary = Salary, Proj_Points = Proj_Points)
+DK_DF = data.frame(
+  LastName=LastName, 
+  FirstName = FirstName, 
+  Position = Position, 
+  Salary = Salary, 
+  Proj_Points = Proj_Points, 
+  Team = Team
+  )
 
 DK_DF = unique(DK_DF)
 
@@ -52,4 +74,20 @@ gsub(' IV', '', DK_DF$LastName, fixed = T)
 gsub(' III', '', DK_DF$LastName, fixed = T)
 gsub(' II', '', DK_DF$LastName = T)
 gsub(' I', '', DK_DF$LastName = T)
+
+
+# filter out DST for each data frame
+def_player_filtered = player_proj %>%  filter(Pos == 'DST')
+def_dk_filtered = DK_DF %>%  filter(Position == 'DST')
+
+#
+# merge on Team into DK_DF
+#
+merged = left_join(dk_filtered, player_filtered, on=Team)
+
+
+
+
+
+
 
