000af6a97a301cf61e86bf0b11cec99fee6a588a
Hugh Goode <hjgoode3@gmail.com>
2021-10-27 20:54:07 -0400

    add lp solver file

--- backend/DK_player_data.R
+++ backend/DK_player_data.R
@@ -7,6 +7,22 @@ week_current <- hist_df %>% filter(Week == 7)
 
 player_proj <- week_current %>% select(Name, Pos, Team, Games.Played, Average.DK.Points)
 
+# Split Name column into first and last name inorder to merge with DK dataframe later
+lastname = sapply(strsplit(player_proj$Name, ','), function(x) x[1])
+firstname = sapply(strsplit(player_proj$Name, ','), function(x) x[length(x)])
+
+# Remove name suffixes to avoid errors when joining data
+lastname = gsub(' IV', '', lastname, fixed = T)
+lastname = gsub(' III', '', lastname, fixed = T)
+lastname = gsub(' II', '', lastname, fixed = T)
+lastname = gsub(' I', '', lastname, fixed = T)
+
+player_proj['Firstname'] = firstname
+player_proj['Lastname'] = lastname
+
+#Rename Def positions to DST in order to match DK data frame data
+player_proj$Pos = gsub('Def', 'DST', player_proj$Pos, fixed = T)
+
 json_file <- 'https://api.draftkings.com/draftgroups/v1/draftgroups/58073/draftables?format=json'
 json_data <- fromJSON(file=json_file)
 
@@ -31,4 +47,9 @@ for(i in 1:length(draftables)){
 DK_DF = data.frame(LastName=LastName, FirstName = FirstName, Position = Position, Salary = Salary, Proj_Points = Proj_Points)
 
 DK_DF = unique(DK_DF)
+
+gsub(' IV', '', DK_DF$LastName, fixed = T)
+gsub(' III', '', DK_DF$LastName, fixed = T)
+gsub(' II', '', DK_DF$LastName = T)
+gsub(' I', '', DK_DF$LastName = T)
 
---
+++ backend/optimizer.R
@@ -0,0 +1,103 @@
+
+#The algorithm takes a csv of player names, positions, salaries, expected points provided by DraftKings, 
+#and expected points scored (rolling average) and generates an optimal lineup 
+#based on the constraints in the evaluation function.
+#
+#The algorithm can be used across a variety of similar linear optimization problems.
+
+#Install.packages("lpsolve")
+
+library(lpSolve)
+
+#Read in dataset
+dataset<-read.csv("INSERT CSV", stringsAsFactors = FALSE)
+
+#Change variables to appropriate types
+dataset$Position <- as.factor(dataset$Position)
+dataset$Salary <-as.numeric(dataset$Salary)
+dataset$Proj_Points <-as.numeric(dataset$Proj_Points)
+dataset$Avg.DK.Points <-as.numeric(dataset$Avg.DK.Points)
+
+#### Prepare constraint matrix of zeros #####
+A <- matrix(0, nrow = 7, ncol = nrow(dataset))
+
+#Designate the positions that are equivalent to each other when generating the optimal lineup
+#There are 6 distinct positions and 1 constraint in which salary is < 50,000
+#I.e. A player with the position WR/RB/TE can fill the FLEX position
+#Add a "1" to all position that can fill that position slot
+
+
+#Set QB parameters
+j<-1
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="QB")
+    A[j,i]<-1
+}
+#RB
+j<-2
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="RB")
+    A[j,i]<-1
+}
+#WR
+j<-3
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="WR")
+    A[j,i]<-1
+}
+#TE
+j<-4
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="TE")
+    A[j,i]<-1
+}
+#FLEX
+j<-5
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="RB"    || 
+      dataset$position[i]=="WR" || 
+      dataset$position[i]== "TE")
+    A[j,i]<-1
+}
+#DST
+j<-6
+i<-1
+for (i in 1:nrow(dataset)){
+  if (dataset$Position[i]=="DST") 
+    A[j,i]<-1
+}
+
+
+i<-1
+
+A[7, ] <- dataset$Salary                # salary <= 50000
+
+# Prepare input for LP solver
+objective.in <- dataset$Proj_Points
+const.mat <- A
+const.dir <- c("==", "==", "==", "==","==","==", "<=")
+const.rhs <- c(1, 2, 3, 1, 1, 1, 50000)
+
+# Generate optimal lineup with lp solve
+require(lpSolve)
+sol <- lp(direction = "max", objective.in, # maximize objective function
+          const.mat, const.dir, const.rhs,   # constraints
+          all.bin = TRUE)                    # use binary variables only
+
+### View the solution
+inds <- which(sol$solution == 1)
+sum(dataset$salary[inds])
+
+
+solution<-dataset[inds, ]
+
+#Print players in optimal lineup
+solution
+
+#Write csv file of the optimal lineup
+write.table(solution1, "mydata.txt", sep="\t")
